4c5602615c2f2f897f1d87ad4235294100389654
naveen652 <73928844+naveen652@users.noreply.github.com>
2023-11-25 20:53:53 +0530

    Delete endpoint directory

--- endpoint/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from .views import sentimentAnalysis
-
-app_name = 'endpoint'
-
-urlpatterns = [
-    path('sentimentAnalysis/', sentimentAnalysis, name='sentimentAnalysis')
-]
