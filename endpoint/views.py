2f35a70c07301c2d41b5b21b66e759b3a96470b6
naveen652 <73928844+naveen652@users.noreply.github.com>
2023-11-27 08:41:19 +0530

    Update views.py

--- endpoint/views.py
+++ endpoint/views.py
@@ -16,7 +16,7 @@ def sentimentAnalysis(request):
     else:
         name=d[0]['name']
         email=d[0]['email']
-        unique_id_data = requests.post('https://mindwellnesspro.onrender.com/reports/'+email)
+        unique_id_data = requests.post('https://mindwellnesspro.onrender.com/reports/'+email+"/")
         unique_id_response = unique_id_data.json()
         unique_id=unique_id_response['UniqueId']
         suggestions="suggestions from genAI"
@@ -24,15 +24,15 @@ def sentimentAnalysis(request):
         responses_df = pd.DataFrame(responses_data)
         questions_data = d[0]['questions']
         #questions_df = pd.DataFrame(questions_data)
-        # concatinated text of questions and responses
+        #concatinated text of questions and responses
         #questions_text = questions_df['Question'].str.cat(sep='\n')
         response_text = responses_df['response'].str.cat(sep='\n ')
-        # list of questions and responses
+        #list of questions and responses
         #list_of_questions = questions_df['Question'].tolist()
         #list_of_responses = responses_df['response'].tolist()
         sentiment_analyzer = pipeline("sentiment-analysis",'arpanghoshal/EmoRoBERTa')
         result = sentiment_analyzer(response_text)
         sentiment_label=result[0]['label']
         sentiment_score=result[0]['score']
-        result_data={'sentiment':sentiment_label, 'score':sentiment_score*100}
+        result_data={'unique id':unique_id,'name':name,'email':email,'sentiment':sentiment_label, 'score':sentiment_score*100,'suggestions':suggestions}
         return JsonResponse(result_data)
