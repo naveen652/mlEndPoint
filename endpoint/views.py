f0753c7d65195179779a646ff8ef7e1d89814710
naveen652 <73928844+naveen652@users.noreply.github.com>
2023-11-27 15:37:41 +0530

    Update views.py

--- endpoint/views.py
+++ endpoint/views.py
@@ -5,6 +5,33 @@ from rest_framework.decorators import api_view
 from transformers import RobertaTokenizerFast, pipeline, RobertaForSequenceClassification
 
 # final model
+def analyze_sentiment_ernie(text):
+    # Load pre-trained ERNIE model and tokenizer
+    tokenizer = ErnieTokenizer.from_pretrained('ernie-1.0')
+    model = ErnieForSequenceClassification.from_pretrained('ernie-1.0')
+
+    # Tokenize the text
+    input_ids = tokenizer(text)['input_ids']
+    input_ids = paddle.to_tensor([input_ids])
+
+    # Make a prediction
+    output = model(input_ids)
+    logits = output.numpy()
+
+    # Get the predicted class and score
+    predicted_class = paddle.argmax(paddle.to_tensor(logits), axis=1).numpy().item()
+    confidence_score = paddle.nn.functional.softmax(paddle.to_tensor(logits), axis=1).numpy()[0, predicted_class]
+
+    # Map numeric label to human-readable sentiment class
+    sentiment_mapping = {
+        0: 'Negative',
+        1: 'Neutral',
+        2: 'Positive'
+    }
+    sentiment_label = sentiment_mapping.get(predicted_class, 'Unknown')
+
+    return sentiment_label, confidence_score
+
 @api_view(['GET'])
 def sentimentAnalysis(request, email):
     response = requests.get('https://mindwellnesspro.onrender.com/userresponse/'+email)
@@ -32,11 +59,8 @@ def sentimentAnalysis(request, email):
         #list of questions and responses
         #list_of_questions = questions_df['Question'].tolist()
         #list_of_responses = responses_df['response'].tolist()
-        tokenizer = RobertaTokenizerFast.from_pretrained("arpanghoshal/EmoRoBERTa",from_tf=True)
-        model = RobertaForSequenceClassification.from_pretrained("arpanghoshal/EmoRoBERTa",from_tf=True)
-        emotion = pipeline('sentiment-analysis', model='arpanghoshal/EmoRoBERTa',from_tf=True)
-        result=emotion(response_text)
-        sentiment_label=result[0]['label']
-        sentiment_score=result[0]['score']
+        result=analyze_sentiment_ernie(response_text)
+        sentiment_label=result[0]['sentiment_label']
+        sentiment_score=result[0]['confidence_score']
         result_data={'unique id':unique_id,'name':name,'email':email,'sentiment':sentiment_label, 'score':sentiment_score*100,'suggestions':suggestions}
         return JsonResponse(result_data)
