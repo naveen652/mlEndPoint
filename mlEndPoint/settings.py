be474a1ddf2ed21a5f01f3542e31b94eaef53ba3
naveen652 <73928844+naveen652@users.noreply.github.com>
2023-11-30 08:42:12 +0530

    Update settings.py

--- mlEndPoint/settings.py
+++ mlEndPoint/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'endpoint'
+    'endpoint',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -48,7 +49,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOWED_ORIGINS=['*']
 
 ROOT_URLCONF = 'mlEndPoint.urls'
 
