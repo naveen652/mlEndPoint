0900dc9fb7942abc852cf5000b61c40bf8807d71
naveen652 <73928844+naveen652@users.noreply.github.com>
2023-11-30 23:18:09 +0530

    Update urls.py

--- endpoint/urls.py
+++ endpoint/urls.py
@@ -4,6 +4,6 @@ from .views import *
 app_name = 'endpoint'
 
 urlpatterns = [
-    path('sentimentAnalysis/<str:email>', sentimentAnalysis, name='sentimentAnalysis'),
+    path('sentimentAnalysis/<str:email>/<int:id>', sentimentAnalysis, name='sentimentAnalysis'),
     path('', health, name='health')
 ]
