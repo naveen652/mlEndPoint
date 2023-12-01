3d272c1689359ed20028fca8c930817c657252df
naveen652 <73928844+naveen652@users.noreply.github.com>
2023-12-01 10:00:23 +0530

    Update views.py

--- endpoint/views.py
+++ endpoint/views.py
@@ -21,32 +21,32 @@ def query(payload, API_URL, headers):
 
 @api_view(['GET'])
 def sentimentAnalysis(request, email):
-    	response = requests.get('https://mindwellnesspro.onrender.com/userresponse/'+email)
-    	d = response.json()
-    	if not d:
-        	return JsonResponse({"error":"no data available"})
-    	#df_json = pd.DataFrame.from_dict(data)
-    	else:
-        	name=d[0]['name']
-        	email=d[0]['email']
-        	suggestions="suggestions from genAI"
-        	responses_data = d[0]['responses']
-        	if responses_data:
-            		unique_id_data = requests.post('https://mindwellnesspro.onrender.com/reports/'+email+"/")
-            		unique_id_response = unique_id_data.json()
-            		unique_id=unique_id_response['UniqueId']
-            		responses_df = pd.DataFrame(responses_data)
-            		#questions_data = d[0]['questions']
-            		response_text = responses_df['response'].str.cat(sep='. ')
-			API_URL = "https://api-inference.huggingface.co/models/finiteautomata/bertweet-base-sentiment-analysis"
-			headers = {"Authorization": "Bearer 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"}
-            		output = query({"inputs": response_text,},API_URL, headers)
-            		sentiments_scores=output[0]
-            		result_data={'unique id':unique_id,'name':name,'email':email,'suggestions':suggestions, "sentiment":sentiment, "sentiments_scores": sentiments_scores, "status":1}
-            		return JsonResponse(result_data)
-        	else:
-            		result_data={'name':name,'email':email, 'error':'no responses'}
-            		return JsonResponse(result_data)
+  response = requests.get('https://mindwellnesspro.onrender.com/userresponse/'+email)
+  d = response.json()
+  if not d:
+    return JsonResponse({"error":"no data available"})
+    #df_json = pd.DataFrame.from_dict(data)
+  else:
+    name=d[0]['name']
+    email=d[0]['email']
+    suggestions="suggestions from genAI"
+    responses_data = d[0]['responses']
+    if responses_data:
+      unique_id_data = requests.post('https://mindwellnesspro.onrender.com/reports/'+email+"/")
+      unique_id_response = unique_id_data.json()
+      unique_id=unique_id_response['UniqueId']
+      responses_df = pd.DataFrame(responses_data)
+      #questions_data = d[0]['questions']
+      response_text = responses_df['response'].str.cat(sep='. ')
+      API_URL = "https://api-inference.huggingface.co/models/finiteautomata/bertweet-base-sentiment-analysis"
+      headers = {"Authorization": "Bearer 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"}
+      output = query({"inputs": response_text,},API_URL, headers)
+      sentiments_scores=output[0]
+      result_data={'unique id':unique_id,'name':name,'email':email,'suggestions':suggestions, "sentiment":sentiment, "sentiments_scores": sentiments_scores, "status":1}
+      return JsonResponse(result_data)
+    else:
+      result_data={'name':name,'email':email, 'error':'no responses'}
+      return JsonResponse(result_data)
 
 @api_view(['GET'])
 def health(request):
